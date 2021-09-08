89d5b470ef55e081cefb5ff52f2da8770c32ad4d
Russell Tsuchida <russell.tsuchida@data61.csiro.au>
2021-09-08 14:01:26 +1000

    basic test without errors

--- tests/tests.py
+++ tests/tests.py
@@ -17,19 +17,19 @@ if __name__ == '__main__':
     plt.scatter(X2[:,0], X2[:,1], c='r', s=1)
 
     # Model and model predictions
-    model = Klr(None)
+    model = Klr(None, precomputed_kernel=False)
     model.fit(X, Y)
     x1 = np.linspace(np.amin(X[:,0]), np.amax(X[:,0]), 100)
     x2 = np.linspace(np.amin(X[:,1]), np.amax(X[:,1]), 100)
 
     xstar = np.transpose([np.tile(x1, len(x2)), np.repeat(x2, len(x1))]) 
-    c, labels = model.predict(xstar)
+    c = model.predict_proba(xstar)
 
     c[np.where(np.isclose(c,0.5, atol=0.05))]=np.nan
     plt.scatter(xstar[:,0], xstar[:,1], c=c, zorder=-5, alpha=0.2)
 
     
     # Accuracy on training data
-    _, Y_predict = model.predict(X)
+    Y_predict = model.predict(X)
     print(np.sum(Y_predict == Y)/Y.shape[0])
     plt.savefig('klr.pdf')
